52d9cef01c506a36cc4cb28887b5204527cc6c2c
garnetgrimm <epicishness@live.com>
2020-05-25 14:02:58 -0400

    my f302r8 exploded so im gonna practice with loading from a file first

---
+++ fromFile/main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "AudioFile.h"
+AudioFile<double> audioFile;
+int main() {
+    audioFile.load ("clean.wav");
+    int sampleRate = audioFile.getSampleRate();
+    int bitDepth = audioFile.getBitDepth();
+
+    int numSamples = audioFile.getNumSamplesPerChannel();
+    double lengthInSeconds = audioFile.getLengthInSeconds();
+
+    int numChannels = audioFile.getNumChannels();
+    bool isMono = audioFile.isMono();
+    bool isStereo = audioFile.isStereo();
+
+    // or, just use this quick shortcut to print a summary to the console
+    audioFile.printSummary();
+    return 0;
+}
---
+++ fromFile/main.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include "AudioFile.h"
+int main() {
+    printf("hello!\r\n");
+    return 0;
+}
